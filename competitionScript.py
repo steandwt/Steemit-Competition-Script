3e91a5f9456d4266b04cfb5f41a81317d2e88197
Stéan de Wit <stean.dewit1@gmail.com>
2017-09-24 18:37:07 +0200

    Update competitionScript.py

--- competitionScript.py
+++ competitionScript.py
@@ -5,18 +5,22 @@ import random
 s = Steem()
 
 #set your username
-user = '<your steemit username>'
+#my username as example
+user = 'benniebanana' 
 #set the link to your post
-postLink = '<permlink to post>'
+#my previous post for example
+postLink = 'getting-started-with-steem-python-upvote-and-comment-bot-examples-linux' 
 
-#get all voters on a post
+#get all the people who voted on the post
 voters = s.get_active_votes(user, postLink)
 #create a list of all the voters
 voterList = [user['voter'] for user in voters]
 #get a list of all the people who reblogged the post
 reblogs = s.get_reblogged_by(user, postLink)
-#get of all your followers
-followers = s.get_followers(user, 0, 'blog', <your follower count or greater>)
+#get of your follower count 
+followerCount = s.get_follow_count(user)['follower_count']
+#retrieve all your followers names
+followers = s.get_followers(user, 0, 'blog',  followerCount)
 #create a list of all your followers
 followerList = [follower['follower'] for follower in followers]
 #create a list of all the people who voted, reblogged and followed you
@@ -28,5 +32,11 @@ for potentialWinner in voterList:
         potentialWinnerList.append(potentialWinner)
 
 #pick a random winner from the list
-winner = random.choice(potentialWinnerList)
-print("yaaay " + winner + " won the competition!!!")
+#check if our list is empty
+if not potentialWinnerList: 
+#our list is empty :(
+    print("No winners :( The list is empty")
+else:
+#we have some potential winners so lets pick a random one
+    winner = random.choice(potentialWinnerList)
+    print("yaaay "+winner+" won the competition!!!")
